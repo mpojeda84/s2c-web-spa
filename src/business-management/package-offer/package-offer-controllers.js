cd35249089c805963c5bd15881f66ac3896e3348
Maikel Pereira <maikel.pereira@gmail.com>
2014-11-02 20:05:46 -0500

    Package Offer by Alejandro

---
+++ src/business-management/package-offer/package-offer-controllers.js
@@ -0,0 +1,38 @@
+/**
+ * Created by amtorres on 10/21/14.
+ */
+businessManagement
+    .controller('packageOfferCtrl', ['$rootScope', '$scope','$log','$modal', '$state', 'businessPersistenceSrv', '$filter',
+        function($rootScope, $scope, $log, $modal, $state, businessPersistenceSrv, $filter) {
+            $scope.businessPersistenceSrv = businessPersistenceSrv;
+
+            $scope.packageOffer = businessPersistenceSrv.newPackageOffer();
+            $scope.index = -1;
+            $scope.packageOffers = businessPersistenceSrv.packageOffers;
+
+            $scope.deletePackageOffer = function(index){
+                $scope.packageOffers.splice(index,1);
+            };
+
+            $scope.editPackageOffer = function(index){
+                $scope.index = index;
+                $scope.packageOffer = angular.copy($scope.packageOffers[index]);
+            };
+
+            $scope.cancelEditPackageOffer = function(index){
+                $scope.index = -1;
+                $scope.packageOffer = angular.copy($scope.packageOffers[index]);
+            };
+
+
+            $scope.savePackageOffer = function(){
+                if($scope.index == -1)
+                    $scope.packageOffers.push($scope.packageOffer);
+                else{
+                    $scope.packageOffers[$scope.index] = $scope.packageOffer;
+                    $scope.index = -1;
+                }
+                $scope.packageOffer = businessPersistenceSrv.newPackageOffer();
+            };
+
+        }]);
---
+++ src/business-management/package-offer/package-offer-services.js
@@ -0,0 +1,8 @@
+/**
+ * Created by amtorres on 10/21/14.
+ */
+
+businessManagement.factory('packageOfferSrv', function() {
+    // basic information specific services, like http calls etc
+});
+
