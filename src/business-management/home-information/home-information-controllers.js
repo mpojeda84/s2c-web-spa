b61e4e4c69046efbdece24beeb48dbe4acea68cf
Alex M <alejandro.mo@outlook.com>
2014-11-04 17:24:09 -0600

    Added Home in Business Management and UI changes.

---
+++ src/business-management/home-information/home-information-controllers.js
@@ -0,0 +1,9 @@
+/**
+ * Created by amtorres on 11/4/2014.
+ */
+businessManagement
+    .controller('homeInformationCtrl', ['$rootScope', '$scope', '$http', '$log','$modal', '$state', 'mapSrv', 'businessPersistenceSrv',
+        function($rootScope, $scope,  $http, $log, $modal, $state, mapSrv, businessPersistenceSrv) {
+
+            $scope.details = '';
+        }]);
