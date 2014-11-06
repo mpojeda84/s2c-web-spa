5988f24e51118c30ecc31876c0d19b2bb4926b96
Alex M <alejandro.mo@outlook.com>
2014-11-05 23:39:27 -0600

    Added badges in menu

--- src/business-management/business-management-controllers.js
+++ src/business-management/business-management-controllers.js
@@ -2,11 +2,18 @@
  * Created by mpereira on 7/30/2014.
  */
 businessManagement
-    .controller('businessManagementCtrl', ['$rootScope', '$scope','$http', '$log','$modal', '$state', 'mapSrv',
-        function($rootScope, $scope,  $http, $log, $modal, $state, mapSrv) {
-            $scope.menu="Initialization (temporal)";
+    .controller('businessManagementCtrl', ['$rootScope', '$scope','$http', '$log','$modal', '$state', 'mapSrv', 'businessPersistenceSrv',
+        function($rootScope, $scope,  $http, $log, $modal, $state, mapSrv, businessPersistenceSrv) {
 
             $scope.$on('$stateChangeSuccess', function(event, toState){
                 $scope.subtitle = toState.data.subtitle;
             })
+
+            $scope.moneyOffers = businessPersistenceSrv.moneyOffers;
+            $scope.packageOffers = businessPersistenceSrv.packageOffers;
+            $scope.paperworkOffers = businessPersistenceSrv.paperworkOffers;
+            $scope.callOffers = businessPersistenceSrv.callOffers;
+            $scope.flightOffers = businessPersistenceSrv.flightOffers;
+
+
         }]);
--- src/personal-management/personal-management-controllers.js
+++ src/personal-management/personal-management-controllers.js
@@ -1,10 +1,11 @@
 
 personalManagement
-    .controller('personalManagementCtrl', ['$rootScope', '$scope','$http', '$log','$modal', '$state', 'mapSrv',
-        function($rootScope, $scope,  $http, $log, $modal, $state, mapSrv) {
-            $scope.menu="Initialization (temporal)";
+    .controller('personalManagementCtrl', ['$rootScope', '$scope','$http', '$log','$modal', '$state', 'mapSrv', 'personalPersistenceSrv',
+        function($rootScope, $scope,  $http, $log, $modal, $state, mapSrv, personalPersistenceSrv) {
 
             $scope.$on('$stateChangeSuccess', function(event, toState){
                 $scope.subtitle = toState.data.subtitle;
             })
+
+            $scope.packageOffers = personalPersistenceSrv.packageOffers;
         }]);
