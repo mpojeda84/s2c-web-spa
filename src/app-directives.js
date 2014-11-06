28c3ffcf26ca6bbcce6df92b8924eca0060513a4
Maikel Pereira <maikel.pereira@gmail.com>
2014-11-05 23:01:39 -0500

    Extended regions list to 3 columns Mocked regions list to 15 elems Added a 'disabled' directive to apply to groups of elements (see it on business-management.html and app-directives) Other minor changes

--- src/app-directives.js
+++ src/app-directives.js
@@ -2,3 +2,20 @@
  * Created by mpereira on 7/30/2014.
  */
 var appDirectives = angular.module('appDirectives',['appServices']);
+
+appDirectives
+.directive('s2cDisabled', function () {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs) {
+            var loadingLayer = angular.element('<div class="s2c_overlay"></div>');
+            element.prepend(loadingLayer);
+            element.addClass('s2c_overlay_container');
+            scope.$watch(attrs.s2cDisabled, function(value) {
+                loadingLayer.toggleClass('ng-hide', !value);
+            });
+        }
+    }
+});
+
--- src/components/region-selector/region-selector-services.js
+++ src/components/region-selector/region-selector-services.js
@@ -6,10 +6,24 @@ regionSelector
         var buildRegions = function() {
 
             return [
-                {id: 1, name: 'Pinar del Rio'},
+                {id: 17, name: 'All'},
+                {id: 1, name: 'Pinar del R.'},
                 {id: 2, name: 'Mayabeque'},
-                {id: 1, name: 'Artemisa'},
-                {id: 1, name: 'La Habana'}
+                {id: 3, name: 'Artemisa'},
+                {id: 4, name: 'La Habana'},
+                {id: 5, name: 'Matanzas'},
+                {id: 6, name: 'I. Juventud'},
+                {id: 7, name: 'Villa Clara'},
+                {id: 8, name: 'Cienfuegos'},
+                {id: 9, name: 'S. Spiritus'},
+                {id: 10, name: 'C. Avila'},
+                {id: 11, name: 'Camaguey'},
+                {id: 12, name: 'Las Tunas'},
+                {id: 13, name: 'Santiago'},
+                {id: 14, name: 'Guantanamo'},
+                {id: 16, name: 'Granma'},
+                {id: 16, name: 'Holguin'}
+
             ];
         };
         return {
